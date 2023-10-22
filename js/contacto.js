1d928d8d3667b32c67cc88946d724c76e97a68be
fer.cipriani <fer.cipriani@gmail.com>
2023-10-22 07:04:48 -0300

    Modificacion Ingrid

--- js/contacto.js
+++ js/contacto.js
@@ -30,3 +30,25 @@ const emailVálido = email => {
 }
 
 sumit.addEventListener('click', validate);
+
+function showError() {
+  if (email.validity.valueMissing) {
+    // Si el campo está vacío
+    // muestra el mensaje de error siguiente.
+    emailError.textContent =
+      "Debe introducir una dirección de correo electrónico.";
+  } else if (email.validity.typeMismatch) {
+    // Si el campo no contiene una dirección de correo electrónico
+    // muestra el mensaje de error siguiente.
+    emailError.textContent =
+      "El valor introducido debe ser una dirección de correo electrónico.";
+  } else if (email.validity.tooShort) {
+    // Si los datos son demasiado cortos
+    // muestra el mensaje de error siguiente.
+    emailError.textContent =
+      "El correo electrónico debe tener al menos ${ email.minLength } caracteres; ha introducido ${ email.value.length }.";
+  }
+
+  // Establece el estilo apropiado
+  emailError.className = "error activo";
+}
